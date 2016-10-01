fb4b95a8fe037ab814cc6ac13b3d0d3881948738
Andrew Bober <abober@performancegateway.com>
2016-10-01 14:21:35 -0500

    renamed variable

--- example/example.js
+++ example/example.js
@@ -20,38 +20,38 @@ var options = {
 	outputDir: "example/generated_jsonschema",
 	resolveURI: "example/generated_jsonschema"
 };
-var bmsConversion = new Xsd2JsonSchema(null, baseDir, filenames, options);
+var converter = new Xsd2JsonSchema(null, baseDir, filenames, options);
 var visitor = null;
 
 if (action === "convert") {
 	visitor = new DefaultConversionVisitor();
-	bmsConversion.processAllSchemas(new DefaultConversionVisitor());
-	bmsConversion.dump();
+	converter.processAllSchemas(new DefaultConversionVisitor());
+	converter.dump();
 } else if (action === "xml-usage") {
 	visitor = new XmlUsageVisitor();
-	bmsConversion.processAllSchemas(visitor);
+	converter.processAllSchemas(visitor);
 	visitor.log();
 } else if (action === "xml-usage-sum") {
 	visitor = new XmlUsageVisitorSum();
-	bmsConversion.processAllSchemas(visitor);
+	converter.processAllSchemas(visitor);
 	visitor.log();
 } else if (action === "test-custom-type") {
 	visitor = new DefaultConversionVisitor();
-	bmsConversion.processAllSchemas(new DefaultConversionVisitor());
-	var jsonSchemas = bmsConversion.getJsonSchemas();
+	converter.processAllSchemas(new DefaultConversionVisitor());
+	var jsonSchemas = converter.getJsonSchemas();
 	var apt = jsonSchemas["ExampleTypes.xsd"].getSubSchemas()["www.xsd2jsonschema.org"].getSubSchemas()["example"].getSubSchemas()["PersonInfoType"];
 	var log = JSON.stringify(apt.getJsonSchema(), null, 2);
 	console.log(log);
 
-	var customTypes = bmsConversion.getCustomTypes();
+	var customTypes = converter.getCustomTypes();
 	var apt2 = customTypes.getNamespace("/www.xsd2jsonschema.org/example").customTypes["PersonInfoType"];
 	var log2 = JSON.stringify(apt2.getJsonSchema(), null, 2);
 	console.log(log2);
 
 } else if (action === "dump-schemas") {
 	visitor = new DefaultConversionVisitor();
-	bmsConversion.processAllSchemas(new DefaultConversionVisitor());
-	bmsConversion.dumpSchemas();
+	converter.processAllSchemas(new DefaultConversionVisitor());
+	converter.dumpSchemas();
 }
 
 
